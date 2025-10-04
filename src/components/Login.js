c042272d3073707342be3dbcf0a2bb48c4aeb0e3
SANJAY KUMAR <124190579+MrBSanjayKumar@users.noreply.github.com>
2025-10-04 13:04:23 +0530

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const validate = () => {
+    let newErrors = {};
+
+    if (!formData.name.trim()) {
+      newErrors.name = "Name is required";
+    }
+
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Email is invalid";
+    }
+
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+    } else if (formData.password.length < 8) {
+      newErrors.password = "Password must be at least 6 characters";
+    }
+
+    return newErrors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({});
+      alert("Form submitted successfully");
+      console.log("Form Data:", formData); // Show form data in console
+      // Local Storage
+      const exitData = localStorage.getItem("formData");
+      const parsedData = exitData ? JSON.parse(exitData) : [];
+      parsedData.push(formData);
+      localStorage.setItem("formData", JSON.stringify(parsedData));
+      
+      // Clear form
+      setFormData({
+        name: "",
+        email: "",
+        password: "",
+      });
+    }
+  };
+
+  return (
+      <div className="form-container">
+        <h2>Form validation</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            {errors.name && <p className="error">{errors.name}</p>}
+          </div>
+
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            {errors.email && <p className="error">{errors.email}</p>}
+          </div>
+
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            {errors.password && <p className="error">{errors.password}</p>}
+          </div>
+
+          <button type="submit" className="btn-submit">
+            Submit
+          </button>
+        </form>
+      </div>
+  );
+};
+export default Login;
